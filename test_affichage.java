34fcf7ee3e7a8e666c457e04f3228d18b488b0c2
Arthur <mermet.arthur@gmail.com>
2024-06-05 14:48:30 +0200

    premier test snake mange la pomme

--- test_affichage.java
+++ test_affichage.java
@@ -13,13 +13,13 @@ public class test_affichage {
         Transition[] T = new Transition[1];
         //Move p = new Move();
         Pick p = new Pick();
-        Cell c = new Cell(DirRelative.soi, cellType.Apple);
+        Cell c = new Cell(DirRelative.soi, cellType.Snake);
         T[0] = new Transition(p, c, 0, 0);
         Automaton a = new Automaton(0, T);
         Pomme item_pomme = new Pomme(g, a);
 
         //
-        /* 
+        
         Transition[] T2 = new Transition[2];
         Cell cond_obsatcle = new Cell(DirRelative.Devant, cellType.Obstacle);
         True cond_true = new True();
@@ -30,7 +30,7 @@ public class test_affichage {
         T2[1] = new Transition(m, cond_true, 0, 0);
         Automaton a2 = new Automaton(0, T2);
         Snake snake = new Snake(a2, g);
-        */
+        
 
 
 
@@ -38,7 +38,7 @@ public class test_affichage {
 
         while (true) {
             a.step_A(item_pomme);
-           // a2.step_A(snake);
+            a2.step_A(snake);
             display d = new display(g);
             d.afficher_grille();
             try {
