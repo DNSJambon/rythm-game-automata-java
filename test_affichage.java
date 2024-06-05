a6801346940ec60d107640b10135a5ab287678ba
Arthur <mermet.arthur@gmail.com>
2024-06-05 16:02:09 +0200

    le snake mange les pommes lets goo

--- test_affichage.java
+++ test_affichage.java
@@ -18,16 +18,22 @@ public class test_affichage {
         Automaton a = new Automaton(0, T);
         Pomme item_pomme = new Pomme(g, a);
 
-        //
-        
-        Transition[] T2 = new Transition[2];
+        //Snake:
+        Transition[] T2 = new Transition[3];
+
         Cell cond_obsatcle = new Cell(DirRelative.Devant, cellType.Obstacle);
         True cond_true = new True();
+        Random r = new Random(33);
+
         Turn gauche = new Turn(DirRelative.Gauche);
         Turn droite = new Turn(DirRelative.Droite);
         Move m = new Move();
+
         T2[0] = new Transition(gauche, cond_obsatcle, 0, 0);
-        T2[1] = new Transition(m, cond_true, 0, 0);
+        T2[1] = new Transition(droite, r, 0, 0);
+        T2[2] = new Transition(m, cond_true, 0, 0);
+        
+
         Automaton a2 = new Automaton(0, T2);
         Snake snake = new Snake(a2, g);
         
@@ -42,7 +48,7 @@ public class test_affichage {
             display d = new display(g);
             d.afficher_grille();
             try {
-                Thread.sleep(250);
+                Thread.sleep(100);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
