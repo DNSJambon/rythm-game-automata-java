79532fc880df4313a0d7272809406fbb1740b367
Aymane Amessegher <aymane.amessegher@etu.univ-grenoble-alpes.fr>
2024-06-05 15:58:21 +0200

    version01graphique

--- Main.java
+++ Main.java
@@ -1,25 +1,19 @@
-
-import view.display;
-
-import model.*;
 import controller.*;
+import model.*;
+import view.*;
+import javax.swing.*;
 
-public class test_affichage {
-    
+public class Main {
     public static void main(String[] args) {
         Grille g = new Grille(10, 10);
 
-        
         Transition[] T = new Transition[1];
-        //Move p = new Move();
         Pick p = new Pick();
         Cell c = new Cell(DirRelative.soi, cellType.Snake);
         T[0] = new Transition(p, c, 0, 0);
         Automaton a = new Automaton(0, T);
         Pomme item_pomme = new Pomme(g, a);
 
-        //
-        
         Transition[] T2 = new Transition[2];
         Cell cond_obsatcle = new Cell(DirRelative.Devant, cellType.Obstacle);
         True cond_true = new True();
@@ -30,17 +24,25 @@ public class test_affichage {
         T2[1] = new Transition(m, cond_true, 0, 0);
         Automaton a2 = new Automaton(0, T2);
         Snake snake = new Snake(a2, g);
-        while (true) {
+
+        // Create and show the GUI
+        SwingUtilities.invokeLater(() -> {
+            view.createAndShowGUI(g);
+        });
+
+        // Use a Swing Timer to periodically update the automaton and repaint the view
+        Timer timer = new Timer(800, e -> {
             a.step_A(item_pomme);
             a2.step_A(snake);
-            display d = new display(g);
-            d.afficher_grille();
-            try {
-                Thread.sleep(250);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-        
+            // Trigger a repaint of the view
+            SwingUtilities.invokeLater(() -> {
+                JFrame topFrame = (JFrame) SwingUtilities.getWindowAncestor(new view(g));
+                if (topFrame != null) {
+                    topFrame.repaint();
+                }
+            });
+        });
+        timer.start();
     }
 }
+
