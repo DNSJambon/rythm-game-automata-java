fe6a584535b668211e25849d2f2776aabceb811b
Matthieu Muller <matthieu.muller2@etu.univ-grenoble-alpes.fr>
2024-06-05 16:34:52 +0200

    Obstacle Terminé

--- model/Obstacle.java
+++ model/Obstacle.java
@@ -1,6 +1,7 @@
 package model;
 
 import controller.Automaton;
+import controller.DirRelative;
 import controller.Direction;
 
 public class Obstacle extends Entity {
@@ -22,9 +23,68 @@ public class Obstacle extends Entity {
     }
 
     @Override
-    public boolean eval_cell(Entity e, Direction dir, cellType type) {
-        return true;
+    public boolean eval_cell(Entity e, DirRelative dir, cellType type) {
+        if (dir==DirRelative.soi) {
+            return type==this.getType();
+        }
+        switch (e.direction) {
+            case Nord:
+                switch (dir) {
+                    case Droite:
+                        return type==g.getCell(x+1%g.getRows(),y).getType();
+                    case Gauche:
+                        return type==g.getCell(x-1%g.getRows(),y).getType();
+                    case Derriere:
+                        return type==g.getCell(x,y+1%g.getCols()).getType();
+                    case Devant:
+                        return type==g.getCell(x,y-1%g.getCols()).getType();
+                    default:
+                        return false;
+                }
+            case Sud:
+                switch (dir) {
+                    case Droite:
+                        return type==g.getCell(x-1%g.getRows(),y).getType();
+                    case Gauche:
+                        return type==g.getCell(x+1%g.getRows(),y).getType();
+                    case Derriere:
+                        return type==g.getCell(x,y-1%g.getCols()).getType();
+                    case Devant:
+                        return type==g.getCell(x,y+1%g.getCols()).getType();
+                    default:
+                        return false;
+                }
+            case Est:
+                switch (dir) {
+                    case Droite:
+                        return type==g.getCell(x,y-1%g.getCols()).getType();
+                    case Gauche:
+                        return type==g.getCell(x,y+1%g.getCols()).getType();
+                    case Derriere:
+                        return type==g.getCell(x-1%g.getRows(),y).getType();
+                    case Devant:
+                        return type==g.getCell(x+1%g.getRows(),y).getType();
+                    default:
+                        return false;
+                }
+            case Ouest:
+                switch (dir) {
+                    case Droite:
+                        return type==g.getCell(x,y+1%g.getCols()).getType();
+                    case Gauche:
+                        return type==g.getCell(x,y-1%g.getCols()).getType();
+                    case Derriere:
+                        return type==g.getCell(x+1%g.getRows(),y).getType();
+                    case Devant:
+                        return type==g.getCell(x-1%g.getRows(),y).getType();
+                    default:
+                        return false;
+                }
+            default:
+                return false;
+        }
     }
+    
     
     @Override
     public boolean do_move(Entity e) {
@@ -33,25 +93,25 @@ public class Obstacle extends Entity {
         switch (e.direction) {
             case Nord:
                 e.y-=1;
-                e.y=e.y%g.getRows();
+                e.y=e.y%g.getCols();
                 g.getCell(xclear,yclear).reset();
                 g.getCell(x,y).setEntity(this);
                 return true;
             case Sud:
                 e.y+=1;
-                e.y=e.y%g.getRows();
+                e.y=e.y%g.getCols();
                 g.getCell(xclear,yclear).reset();
                 g.getCell(x,y).setEntity(this);
                 return true;
             case Est:
                 e.x+=1;
-                e.x=e.x%g.getCols();
+                e.x=e.x%g.getRows();
                 g.getCell(xclear,yclear).reset();
                 g.getCell(x,y).setEntity(this);
                 return true;
             case Ouest:
                 e.x-=1;
-                e.x=e.x%g.getCols();
+                e.x=e.x%g.getRows();
                 g.getCell(xclear,yclear).reset();
                 g.getCell(x,y).setEntity(this);
                 return true;
@@ -94,20 +154,55 @@ public class Obstacle extends Entity {
     }
 
     @Override
-    public boolean do_turn(Entity e) {
+    public boolean do_turn(Entity e,DirRelative dir) {
+        if (dir==DirRelative.soi || dir==DirRelative.Devant){
+            return true;
+        }
         switch (e.direction) {
             case Nord:
-                e.direction=Direction.Est;
-                return true;
+                switch (dir) {
+                    case Droite:
+                        e.direction=Direction.Est;
+                    case Gauche:
+                        e.direction=Direction.Ouest;
+                    case Derriere:
+                        e.direction=Direction.Sud;
+                    default:
+                        return false;
+                }
             case Sud:
-                e.direction=Direction.Est;
-                return true;
+                switch (dir) {
+                    case Droite:
+                        e.direction=Direction.Ouest;
+                    case Gauche:
+                        e.direction=Direction.Est;
+                    case Derriere:
+                        e.direction=Direction.Nord;
+                    default:
+                        return false;
+                }
             case Est:
-                e.direction=Direction.Est;
-                return true;
+                switch (dir) {
+                    case Droite:
+                        e.direction=Direction.Sud;
+                    case Gauche:
+                        e.direction=Direction.Nord;
+                    case Derriere:
+                        e.direction=Direction.Ouest;
+                    default:
+                        return false;
+                }
             case Ouest:
-                e.direction=Direction.Est;
-                return true;
+                switch (dir) {
+                    case Droite:
+                        e.direction=Direction.Nord;
+                    case Gauche:
+                        e.direction=Direction.Sud;
+                    case Derriere:
+                        e.direction=Direction.Est;
+                    default:
+                        return false;
+                }
             default:
                 return false;
         }
