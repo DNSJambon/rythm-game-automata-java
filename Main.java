bf6b2a53cca61394874f50a0d4f1d737f1d493e8
Matthieu Muller <matthieu.muller2@etu.univ-grenoble-alpes.fr>
2024-06-06 12:09:02 +0200

    Reset Main

--- Main.java
+++ Main.java
@@ -7,58 +7,40 @@ public class Main {
     public static void main(String[] args) {
         Grille g = new Grille(10, 10);
 
-        //Pomme:
-        Transition[] T = new Transition[3];
-
-        Cell c1 = new Cell(DirRelative.Devant, cellType.Vide); 
-
-        Egg e1 = new Egg();
-
-        Cell c2 = new Cell(DirRelative.Devant, cellType.Obstacle);
-
-        Turn t2 = new Turn(DirRelative.Derriere);
-
-        Cell c3 = new Cell(DirRelative.Derriere, cellType.Obstacle);
-
-        Move m3 = new Move();
-
-        T[0] = new Transition(m3, c3, 0, 0);
-        T[1] = new Transition(t2, c2, 0, 0);
-        T[2] = new Transition(e1, c1, 0, 0);
-
+        Transition[] T = new Transition[1];
+        Pick p = new Pick();
+        Cell c = new Cell(DirRelative.soi, cellType.Snake);
+        T[0] = new Transition(p, c, 0, 0);
         Automaton a = new Automaton(0, T);
-        Obstacle obs = new Obstacle(g, a,2,3);
+        Pomme item_pomme = new Pomme(g, a);
 
-        //Snake:
-        Transition[] T2 = new Transition[4];
-
+        Transition[] T2 = new Transition[2];
         Cell cond_obsatcle = new Cell(DirRelative.Devant, cellType.Obstacle);
         True cond_true = new True();
-        Random r = new Random(10);
-
         Turn gauche = new Turn(DirRelative.Gauche);
         Turn droite = new Turn(DirRelative.Droite);
         Move m = new Move();
-
         T2[0] = new Transition(gauche, cond_obsatcle, 0, 0);
-        T2[1] = new Transition(droite, r, 0, 0);
-        T2[2] = new Transition(gauche, r, 0, 0);
-        T2[3] = new Transition(m, cond_true, 0, 0);  
-
+        T2[1] = new Transition(m, cond_true, 0, 0);
         Automaton a2 = new Automaton(0, T2);
         Snake snake = new Snake(a2, g);
-
 
         // Create and show the GUI
         SwingUtilities.invokeLater(() -> {
             view.createAndShowGUI(g);
         });
 
- 
-        // Use a Swing Timer to periodically update the automaton
-        Timer timer = new Timer(250, e -> {
-            a.step_A(obs);
+        // Use a Swing Timer to periodically update the automaton and repaint the view
+        Timer timer = new Timer(800, e -> {
+            a.step_A(item_pomme);
             a2.step_A(snake);
+            // Trigger a repaint of the view
+            SwingUtilities.invokeLater(() -> {
+                JFrame topFrame = (JFrame) SwingUtilities.getWindowAncestor(new view(g));
+                if (topFrame != null) {
+                    topFrame.repaint();
+                }
+            });
         });
         timer.start();
     }
