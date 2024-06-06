497160681e73ee0b7bed4c385be6c16ee70eaabc
Matthieu Muller <matthieu.muller2@etu.univ-grenoble-alpes.fr>
2024-06-06 16:16:02 +0200

    buffer comment

--- info3.game/game/info3/game/controller/BufferAction.java
+++ info3.game/game/info3/game/controller/BufferAction.java
@@ -1,9 +1,13 @@
 package info3.game.controller;
 
 public class BufferAction {
+    /* ici on construit un buffer d'action pour permettre au moteur de resoudre toutes les actions en meme temps 
+    (et donc garder le "rythme" ou la "synchroniqation") */
     Action action[];
     int size;
     int index;
+
+    /* Le buffer doit avoir autant d'action que d'entité qui existe sur le moteur */
 
     public BufferAction(int size) {
         this.size = size;
@@ -11,6 +15,7 @@ public class BufferAction {
         this.index = 0;
     }
 
+    /* Ajoute une action au buffer */
     public void addAction(Action action) {
         if (index < size) {
             this.action[index] = action;
@@ -18,6 +23,7 @@ public class BufferAction {
         }
     }
 
+    /* Resout toutes les actions d'un coup */
     public void resolve() {
         for (int i = 0; i < index; i++) {
             action[i].exec(action[i].e_or);
