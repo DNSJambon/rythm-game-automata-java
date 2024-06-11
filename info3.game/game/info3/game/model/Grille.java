e692d3acdb31f6488fc1f991c1590ba40ca2c9f4
Arthur <mermet.arthur@gmail.com>
2024-06-11 18:14:43 +0200

    observation rapide

--- info3.game/game/info3/game/model/Grille.java
+++ info3.game/game/info3/game/model/Grille.java
@@ -92,9 +92,6 @@ public class Grille implements IGrille{
             }
         }
         
-        
-        
-
         Collections.shuffle(emptyCells, random);
         // System.out.println("print cells : "+emptyCells);
         boolean[][] tempObstacles = new boolean[grille.rows][grille.cols];
@@ -205,6 +202,7 @@ public class Grille implements IGrille{
 
 
     /* ======================Partie Synchro========================== */
+    //TODO on ne peux pas jouer le tour suivant apres en avoir sauté un
     public char getTouche() {
         return touche;
     }
