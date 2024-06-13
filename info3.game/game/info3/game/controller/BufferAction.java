582563724be0c08e032d940a18b80ef2cced9a41
Matthieu Muller <matthieu.muller2@etu.univ-grenoble-alpes.fr>
2024-06-13 12:32:51 +0200

    récupération mode

--- info3.game/game/info3/game/controller/BufferAction.java
+++ info3.game/game/info3/game/controller/BufferAction.java
@@ -1,20 +1,19 @@
 package info3.game.controller;
 
-import info3.game.controller.Actions.Action;
 import info3.game.model.Entities.Entity;
+import info3.game.controller.Mode;
 
 public class BufferAction {
     /* ici on construit un buffer d'action pour permettre au moteur de resoudre toutes les actions en meme temps 
-     * (et donc garder le "rythme" ou la "synchroniqation").
+     * (et donc garder le "rythme" ou la "synchronisation").
     */
     Act act[];
     int size;
     int index;
-    public Entity main_entity; //TODO: A supprimer
 
     /* Prend en paramètre la taille du buffer a construire.
-     * Le buffer doit avoir autant d'action que d'entité qui existe sur le moteur ou qu'il y a a faire.
-     * Il est possible de faire un buffer plus grand que le nombre d'entité, mais pas plus petit car on y perdrais des actions.
+     * Le buffer doit avoir autant de mode que d'entité qui existe sur le moteur ou qu'il y a a faire.
+     * Il est possible de faire un buffer plus grand que le nombre d'entité, mais pas plus petit car des entités perdarit alors leurs actions.
     */
 
     public BufferAction(int size) {
@@ -23,36 +22,29 @@ public class BufferAction {
         this.index = 0;
     }
 
-    /* Ajoute une action au buffer.
-     * Prend en paramètre une action.
-     * Si Action=null, buffer inchangé.
+    /* Ajoute un mode au buffer.
+     * Prend en paramètre un mode.
+     * Si mode=null, buffer inchangé.
      * Si buffer plein, action non ajoutée.
     */
-    public void addAct(Entity e, Transitions T) {
-        if (T != null) {
+    public void addAct(Entity e, Mode mode) {
+        if (mode != null) {
             if (index < size) {
-                this.act[index] = new Act(e, T.act, T.cond);
+                this.act[index] = new Act(e, mode);
                 index++;
             }
         }
     }
 
     /* Resout toutes les actions du buffer.
-     * Chaque action est resolue sur son entité d'origine.
-     * ( possibilité d'ajouté une entité cible en paramètre pour resoudre les actions sur une entité spécifique)
+     * pour chaque mode enregistré, On resout l'action.
      * 
     */
 
     public void resolve() {
         for (int i = 0; i < index; i++) {
             if (this.act!=null) {
-                if (act[i].cond.eval(act[i].owner)) {
-                    act[i].action.exec(act[i].owner);
-                }
-                else {
-                    System.out.println("Condition non remplie");
-                    i++;
-                }
+                this.act[i].resolve();
             }
         }
         index = 0;
