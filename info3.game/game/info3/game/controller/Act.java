27a7b641c5497f80728ac7f2190cb8c29a4e6039
Matthieu Muller <matthieu.muller2@etu.univ-grenoble-alpes.fr>
2024-06-13 12:49:08 +0200

    Buffer a tester

--- info3.game/game/info3/game/controller/Act.java
+++ info3.game/game/info3/game/controller/Act.java
@@ -1,33 +1,22 @@
 package info3.game.controller;
 
 import info3.game.model.Entities.Entity;
-import info3.game.controller.Mode;
+import info3.game.controller.Modes;
 
 public class Act {
     Entity owner;
-    Mode mode;
+    Modes mode;
 
-    public Act(Entity owner, Mode mode) {
+    public Act(Entity owner, Modes mode) {
         this.owner = owner;
         this.mode = mode;
     }
 
     public void resolve() {
         if (mode != null) {
-            while (mode..get(i)!=null) {
-                if (e.etat_courant.equals(Trans.get(i).init)) {
-                    if (Trans.get(i).cond.eval(e)) {
-                        e.etat_courant = Trans.get(i).end;
-                        buff.addAction(Trans.get(i).act);
-                        return true;
-                    }
-                }
-                i++;
-                if (i== Trans.size()) {
-                    return false;
-                }
+            if (mode.getTrans(owner) != null) {
+                mode.getTrans(owner).GetAct().exec(owner);
             }
-            return true;
         }
     }
 
