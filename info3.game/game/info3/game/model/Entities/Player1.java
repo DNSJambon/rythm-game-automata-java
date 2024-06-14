c8dc54c9ec75d4cee93e9de477f67750b15e2fac
Omar Elfarouk Chtioui <omar-elfarouk.chtioui@etu.univ-grenoble-alpes.fr>
2024-06-14 12:20:36 +0200

    Update Player1.java

--- info3.game/game/info3/game/model/Entities/Player1.java
+++ info3.game/game/info3/game/model/Entities/Player1.java
@@ -28,7 +28,7 @@ public class Player1 extends Entity{
         this.y = y;
 
          try {
-            m_images = Grille.loadSprite("resources/squelette.png", 1, 4);
+            m_images = Grille.loadSprite("resources/magesquelette.png", 1, 4);
         } catch (IOException e) {
             e.printStackTrace();
         }
