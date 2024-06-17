17bf1ea889f0db798a9d0206bfa696608e28c083
Arthur <mermet.arthur@gmail.com>
2024-06-17 11:14:41 +0200

    color minimap

--- info3.game/game/info3/game/model/Grille.java
+++ info3.game/game/info3/game/model/Grille.java
@@ -421,6 +421,9 @@ public class Grille implements IGrille {
                     case 'T':
                         g.setColor(player);
                         break;
+                    case 'E':
+                        g.setColor(Color.YELLOW);
+                        break;
                     case '#':
                         g.setColor(Color.BLUE);
                         break;
