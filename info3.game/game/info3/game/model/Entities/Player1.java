d25fc3db1217e1088373fe68b7f7007a416c597b
Arthur <mermet.arthur@gmail.com>
2024-06-17 23:03:26 +0200

    calibration

--- info3.game/game/info3/game/model/Entities/Player1.java
+++ info3.game/game/info3/game/model/Entities/Player1.java
@@ -150,8 +150,8 @@ public class Player1 extends Entity{
     @Override
     public void tick(long elapsed) {
         if (in_hit) {
-            hit_anim +=1;
-            if (hit_anim > 70) {
+            hit_anim +=elapsed;
+            if (hit_anim > 35) {
                 hit_anim = 0;
                 image_hit = (image_hit + 1);
                 
